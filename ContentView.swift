88a2e202b62529c5e0200cbf8c670eb1ea3baad1
Aleks Todorov <atodorov6421@stu.d214.org>
2025-04-10 10:56:45 -0500

    added a variable and a text showing the variable

--- ContentView.swift
+++ ContentView.swift
@@ -3,11 +3,15 @@ import SwiftUI
 struct ContentView: View {
     @State var number2 : Int = 0
     @State var number1 : Int = 0
+    @State var answer = ""
     var body: some View {
+        
         TextField("Enter number 1", value: $number1, format: .number)
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .padding()
         TextField("Enter number 2", value: $number2, format: .number)
             .textFieldStyle(RoundedBorderTextFieldStyle())
             .padding()
-        
+        Text(answer)
     }
 }
